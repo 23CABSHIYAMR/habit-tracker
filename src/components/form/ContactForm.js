fdbfa4988f0c6a0644ace878d686febd68ab9abd
Rahul-20x03 <rahuldev1320@gmail.com>
2023-09-29 13:51:21 +0530

    Project completed without backend

--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -1,28 +1,20 @@
 import React from "react";
 import "../css/style.css";
 import {GoPaperAirplane}from'react-icons/go'
+var className = `w-full h-[50px] rounded-lg px-3 py-2 bg-[transparent] border-[#949494]
+border placeholder:font-medium placeholder:leading-8
+placeholder:text-[#d6d6d6b3]
+placeholder:text-h6 focus:outline-none`;
 function ContactForm() {
   return (
     <section className="text-white2 text-h6 font-[300] leading-1 my-4 w-full sm:my-4 tiny:my-4 md:my-4">
       <h3 className="text-h2 text-white1 font-bold my-2">Contact Form</h3>
       <div className="w-full grid grid-cols-2 sm:grid-cols-1 gap-4 tiny:grid-cols-1 md:grid-cols-2 ">
         <div className="w-full my-4">
-          <input
-            className="w-full h-[50px] rounded-lg px-3 py-2 bg-[transparent] border-[#949494]
-       border placeholder:font-medium placeholder:leading-8
-       placeholder:text-[#d6d6d6b3]
-       placeholder:text-h6 focus:outline-none"
-            placeholder="Full name"
-          />
+          <input className={className} placeholder="Full name" />
         </div>
         <div className="w-full my-4">
-          <input
-            className="w-full h-[50px] rounded-lg px-3 py-2 bg-[transparent] border-[#949494]
-       border placeholder:font-medium placeholder:leading-8
-       placeholder:text-[#d6d6d6b3]
-       placeholder:text-h6 focus:outline-none"
-            placeholder="Email Address"
-          />
+          <input className={className} placeholder="Email Address" />
         </div>
       </div>
       <div className="w-full grid grid-cols-1 sm:grid-cols-1 gap-4 tiny:grid-cols-1 md:grid-cols-1 ">
